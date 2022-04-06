b46932b44a10320e3770b7403bc063ee2e36429b
Balazs9 <sempergerbalazs@gmail.com>
2022-04-06 21:15:29 +0000

    added products template and product views

--- products/models.py
+++ products/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=254)
     product_type = models.CharField(max_length=254, null=True, blank=True)
 
