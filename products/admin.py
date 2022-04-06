833f077c4ad73530f8039ab1423f0ff3e9301945
Balazs9 <sempergerbalazs@gmail.com>
2022-04-06 21:00:31 +0000

    added product admin and category admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Product, Category
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'Category',
+        'price',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_type',
+        'name',
+    )
+
 admin.site.register(Product)
 admin.site.register(Category)
