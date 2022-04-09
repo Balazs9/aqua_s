25832afd97266bcd085ce6e560333b8922358827
Balazs9 <sempergerbalazs@gmail.com>
2022-04-09 10:15:33 +0000

    added product containers in product templates

--- products/views.py
+++ products/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Product, Category
+from django.contrib import messages
+from django.db.models import Q
 
 
 def all_products(request):
     """ A view to show all products """
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
 
     products = Product.objects.all()
 
