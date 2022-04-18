88be61381875a9edf99c32200e9d54f0d7101f54
Balazs9 <sempergerbalazs@gmail.com>
2022-04-18 10:05:37 +0000

    viewing product detail

--- products/views.py
+++ products/views.py
@@ -19,40 +19,40 @@ def all_products(request):
 def mineral_water(request):
     """ A view to show only the mineral waters """
 
-    products = Product.objects.filter(category__name=mineral_water)
+    product = Product.objects.filter(category__name='mineral_water')
 
     return render(request, 'products/products.html', {
-        'products': products,
+        'product': product,
     })
 
 
 def sport_drink(request):
     """ A view to show only the sport drinks """
 
-    products = Product.objects.filter(category__name=sport_drink)
+    product = Product.objects.filter(category__name='sport_drink')
 
     return render(request, 'products/products.html', {
-        'products': products,
+        'product': product,
     })
 
 
 def water_dispenser(request):
     """ A view to show only the water dispenser machines """
 
-    products = Product.objects.filter(category__name=water_dispenser)
+    product = Product.objects.filter(category__name='water_dispenser')
 
     return render(request, 'products/products.html', {
-        'products': products,
+        'product': product,
     })
 
 
 def product_detail(request, product_id):
     """ A view to show informations about the product """
 
-    products = get_object_or_404(Product, pk=product_id)
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': products,
+        'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
