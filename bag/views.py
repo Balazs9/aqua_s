f8f22581c1451a78300e75e610e194d6e997a66f
Balazs9 <sempergerbalazs@gmail.com>
2022-04-18 16:05:17 +0000

    fixed background image for main page, adjust css

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
 from products.models import Product
 
 # Create your views here.
@@ -18,5 +20,48 @@ def add_to_bag(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity in your basket!')
+    else:
+        bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your basket!')
+
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the chosen product in the bag """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity in your basket!')
+    else:
+        bag[item_id] = quantity
+        messages.success(request, f'Removed {product.name} quantity in your basket!')
+
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ remove the product from the bag """
+
+    product = get_object_or_404(Product, pk=item_id)
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+        messages.success(request, f'Deleted {product.name} from your basket!')
+
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
