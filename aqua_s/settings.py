325c8a6c8cb1d2d38b37840a236e0c7bbef9adc2
Balazs9 <sempergerbalazs@gmail.com>
2022-06-07 21:16:05 +0000

    fixed default from email in settings.py

--- aqua_s/settings.py
+++ aqua_s/settings.py
@@ -220,11 +220,11 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'aqua_s@example.com'
+
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'aquas@example.com'
+    DEFAULT_FROM_EMAIL = 'aqua_s@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
